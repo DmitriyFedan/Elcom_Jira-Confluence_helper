60f7668f84c1dd14e2e8ba4d60be3709a659031d
DmitriyFedan <dmitry061093@mail.ru>
2023-02-01 00:44:53 +0700

    reading Discord token from Administration folder

--- DiscordElcrumPokerBot/ElcomPokerBotDiscord.py
+++ DiscordElcrumPokerBot/ElcomPokerBotDiscord.py
@@ -1,13 +1,20 @@
 ##pip install --proxy http://fedanda:5u738mbcyopE3@192.168.0.10:8080 Jira
 
 print ("1#####")
+
 import discord
+import os
+
 from discord.ext import commands
 
 
-Token = ""
+Token = ""  ## put your token or read from file
 
-with open("C:\\Users\\FedanDA\\Desktop\\FEDANDA\\My repos\\Scrum Helpers\\Administration\\DiscordBotToken.txt", "r") as file:
+##  Reading  Discord Token from  Administrationfolder
+curentDirectoryPath = os.path.abspath(os.curdir)
+tokenPath = curentDirectoryPath + "\Administration\DiscordBotToken.txt"
+
+with open(tokenPath, "r") as file:
     Token = file.read()
 
 config = {
@@ -18,7 +25,7 @@ config = {
 intents = discord.Intents.default() # Подключаем "Разрешения"
 intents.message_content = True
 # Задаём префикс и интенты
-bot = commands.Bot(command_prefix='/', intents=intents, proxy = "http://fedanda:5u738mbcyopE4@192.168.0.10:8080" )    ##  , intents=intents, proxy_auth = "http://fedanda:5u738mbcyopE4@192.168.0.10:8080"
+bot = commands.Bot(command_prefix='/', intents=intents )    ##  , intents=intents, proxy_auth = "http://fedanda:5u738mbcyopE4@192.168.0.10:8080" proxy = "http://fedanda:5u738mbcyopE4@192.168.0.10:8080"
 
 print ("2#####")
 # С помощью декоратора создаём первую команду
