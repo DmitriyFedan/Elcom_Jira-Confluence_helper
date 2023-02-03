3b8be2f8cc1ccdc2cb823ba371106cf959bb1ae4
ELCOM\FedanDA <FedanDA@elcomplus.ru>
2023-02-04 00:53:08 +0700

    add gitignor (debug and VS )

--- DiscordElcrumPokerBotDotNet/ElcrumPokerBotDiscord/Program.cs
+++ DiscordElcrumPokerBotDotNet/ElcrumPokerBotDiscord/Program.cs
@@ -2,6 +2,21 @@
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord.Net.Rest;
+using Discord.Net.Udp;
+using Discord.Net.WebSockets;
 
 
 namespace ElcrumPokerBotDiscord
@@ -15,18 +30,43 @@ namespace ElcrumPokerBotDiscord
         List<string> AvailableClients = new List<string>()
             {
                 "Vladimir Shtarev",
-                "Dmitriy_Fedan"
+                "Dmitriy_Fedan",
+                "Chjen Nikita"
 
             };
         static void Main(string[] args)
         => new Program().MainAsync().GetAwaiter().GetResult();
 
 
-
+        public static class ElcomWebSocketProvider
+        {
+            public static readonly WebSocketProvider Instance = DefaultWebSocketProvider.Create();
+        }
 
         private async Task MainAsync()
         {
-            DiscordSocketClient client = new DiscordSocketClient();
+
+            DiscordSocketConfig socketConfig = new DiscordSocketConfig()
+            {
+                //https://stackoverflow.com/questions/29856543/httpclient-and-using-proxy-constantly-getting-407
+                //RestClientProvider = DefaultRestClientProvider.Create(true), // или скорее всего придется сделать свой класс типа DefaultRestClient, только чтобы запросы шли через прокси
+                RestClientProvider = ElcomRestClientProvider.Instance,
+                UdpSocketProvider = DefaultUdpSocketProvider.Instance, //тут похоже нужно добавить для Udp прокси, пока хз как это делать
+                WebSocketProvider = DefaultWebSocketProvider.Create(new WebProxy
+                    {
+                            Address = new Uri($"http://192.168.0.10:8080"),
+                            BypassProxyOnLocal = false,
+                            UseDefaultCredentials = false,
+
+                            // *** These creds are given to the proxy server, not the web server ***
+                            Credentials = new NetworkCredential(
+                                userName: "fedanda",
+                                password: "5u738mbcyopE4")
+                    }),
+        
+            };
+
+            DiscordSocketClient client = new DiscordSocketClient(socketConfig);
 
             
 
@@ -40,13 +80,13 @@ namespace ElcrumPokerBotDiscord
             path = String.Concat(path, "\\Administration\\DiscordBotToken.txt");
 
 
-            using (StreamReader reader = new StreamReader("G:\\WORKED\\Python\\Elcrum Helpers\\Administration\\DiscordBotToken.txt"))
+            using (StreamReader reader = new StreamReader("C:\\Users\\FedanDA\\Desktop\\FEDANDA\\My repos\\Scrum Helpers\\Administration\\DiscordBotToken.txt"))
             {
                 token = await reader.ReadToEndAsync();
 
             }
 
-                await client.LoginAsync(TokenType.Bot, token);
+            await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
 
             Console.ReadLine();
